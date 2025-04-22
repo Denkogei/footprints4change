e323472f5d3886eae12165f53041c2c9cd650ea5
Dennis Kemboi <denkogei11@gmail.com>
2025-04-22 13:44:13 +0300

    modified staff component

--- footprints4change/src/App.jsx
+++ footprints4change/src/App.jsx
@@ -4,10 +4,8 @@ import Home from "./Components/Home";
 import Initiatives from "./Components/Initiatives";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
-import Members from "./Components/Members";
 import GetInvolved from "./Components/GetInvolved";
-import Directors from "./Components/Directors";
-import Staff from "./Components/StaffMembers";
+import Staff from "./Components/Staff";
 import Footer from "./Components/Footer";
 import Donate from "./Components/Donate";
 
@@ -19,12 +17,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/initiatives" element={<Initiatives />} />
         <Route path="/about" element={<About />} />
-        <Route path="/members" element={<Members />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/get-involved" element={<GetInvolved />} />
         <Route path="/donate" element={<Donate />} />
-        <Route path="/members/directors" element={<Directors />} />
-        <Route path="/members/staff" element={<Staff />} />
+        <Route path="/staff" element={<Staff />} />
       </Routes>
       <Footer />
     </Router>
